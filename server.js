df350c28dbde9715f1e8d06b2705a7e5ddf44a15
Deadpool1 <atwood169@yahoo.com>
2022-07-09 11:28:35 -0500

    created server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+const { db } = express('./db/db');
+
+
+app.get('/api/db', (req, res) => {
+    res.send('Hello!');
+  });
+
+app.listen(3001, () => {
+  console.log(`API server is now on Port 3001`);  
+});
