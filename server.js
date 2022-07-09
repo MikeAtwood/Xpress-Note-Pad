60a6c0e54abc89983d2c2b56fd98f7800dea0737
Deadpool1 <atwood169@yahoo.com>
2022-07-09 17:39:36 -0500

    added Parse json

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require('express');
+//-------- Dependencies ---------//
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+//--------- Sets up Express App ---------//
 const app = express();
-const { db } = express('./db/db');
+const PORT = process.env.PORT || 3001;
 
 
 app.get('/api/db', (req, res) => {
-    res.send('Hello!');
+    res.send('database');
   });
 
+//--------- Parse data ----------//
+app.use(express.json());
+
+//--------- Sets up App Listener ---------//
 app.listen(3001, () => {
   console.log(`API server is now on Port 3001`);  
 });
